38687e719daeaeab62c110cb63d3e3fb1c437444
Elena Mannoni <elemannoni04@gmail.com>
2025-09-07 19:04:50 +0200

    Add files via upload

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ import tracemalloc
 from copy import deepcopy
 
 from .train_and_evaluation import train_model, evaluate_model
-from .utils import freeze_layers_selectively
+from .utils import freeze_layers_selectively, transfer_body_weights
 
 def finetuning_experiments(model, model_A, model_B, merged_models_to_evaluate, loader_full_train, loader_full_test, device, is_alignment_noop, 
                                epochs_base=10, lr_base=1e-4, epochs_merged=10, lr_merged=1e-4):
